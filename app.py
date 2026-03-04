15f48249e4abeb249af9cdc5e62ac3a57140ba40
hilalkhannasir <hilal4294704@gmail.com>
2026-03-04 21:41:41 +0500

    Completed Frontend+Backend

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify,send_from_directory
+import os
+from data_retrieval import get_answer_from_query
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return send_from_directory("static", "chat.html")
+
+@app.route("/chat", methods=["GET","POST"])
+def chat():
+    data = request.json
+    user_query = data.get("message", "").strip()
+
+    if not user_query:
+        return jsonify({"error": "Empty message"}), 400
+
+    try:
+        answer, source = get_answer_from_query(user_query)
+        return jsonify({
+            "answer": answer,
+            "source": source
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080,debug=True)
---
+++ data_retrieval.py
@@ -0,0 +1,55 @@
+import os
+from groq import Groq
+from tavily import TavilyClient
+from langchain_core.prompts import PromptTemplate
+from pinecone import Pinecone
+from sentence_transformers import SentenceTransformer
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = SentenceTransformer('sentence-transformers/all-roberta-large-v1')
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+index = pc.Index(name=os.getenv("INDEX_NAME"), host=os.getenv("HOST_NAME"))
+def generate_embeddings(texts):
+    return model.encode(texts)
+
+def get_response_from_lama(prompt: str, model: str = "openai/gpt-oss-20b") -> str:
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[{"role": "user", "content": prompt}],
+            model=model,
+        )
+        return chat_completion.choices[0].message.content
+    except Exception as e:
+        raise ValueError(f"Groq API call failed: {e}")
+
+def get_answer_from_query(user_query):
+  template = """Analyze the following Question and give one word answer of YES if the question is about pakistan history or about the mughal empire,
+  about the events that occured in indian subcontinent after mughal empire and before partition of the subcontinent and formation of Pakistan.
+   answer with NO otherwise. Don't over-format the result. Keep it Simple, concise like O level exam style answers. Don't add <br> tokens.Question: {query}"""
+
+  prompt = PromptTemplate(input_variables=["query"],template=template)
+  final_prompt = prompt.format(query=user_query)
+  response = get_response_from_lama(final_prompt)
+  if response == "NO":
+    return ["Query was not About Pakistan History","Error"]
+
+  query_embedding = generate_embeddings([user_query])[0]
+  query_result = index.query(vector=query_embedding.tolist(), top_k=3)
+  reply_from = ''
+  if query_result['matches'][0]['score']>0.65:
+    context = "\n".join([" ".join(match['values']) for match in query_result['matches']])
+    reply_from = 'Book'
+  else:
+    reply = tavily_client.search(user_query)
+    relevant_content = [i["content"] for i in reply["results"] if i.get("score",0) > 0.65]
+    context = "\n\n".join(relevant_content)
+    reply_from = 'Internet Search'
+
+  llama_prompt = f"Context:\n{context}\n\nUser Query: {user_query}\nAnswer:"
+  response = get_response_from_lama(llama_prompt)
+  return response,reply_from
